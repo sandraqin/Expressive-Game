ca2883c56acfb65ac95a5a97b9db72ecc70b58ef
Sandra Qin <qinq005@newschool.edu>
2023-04-11 23:28:32 -0400

    aa

---
+++ main.js
@@ -0,0 +1,8 @@
+function showDiv() {
+    var rule = document.getElementById('rule');
+    if (rule.style.display === "none") {
+      rule.style.display = "block";
+    } else {
+      rule.style.display = "none";
+    }
+  }
